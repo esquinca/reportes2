4a69432d79ca823134596e1dcf712f72e8117c2e
Alonso Cauich <alonsocauichv1@gmail.com>
2018-02-06 14:32:48 -0500

    modal

--- resources/views/permitted/report/view_approval_concierge.blade.php
+++ resources/views/permitted/report/view_approval_concierge.blade.php
@@ -26,6 +26,65 @@
 
 @section('content')
     @if( auth()->user()->can('View concierge approval') )
+    <div class="modal modal-default fade" id="add_approval" data-backdrop="static">
+        <div class="modal-dialog" >
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              <h4 class="modal-title"><i class="fa fa-id-card-o" style="margin-right: 4px;"></i>{{ trans('message.title_admin_approval') }}</h4>
+            </div>
+            <div class="modal-body">
+              <div class="box-body table-responsive">
+                <div class="box-body">
+                  <div class="row">
+                  @if( auth()->user()->can('Create concierge approval') )
+                    <div class="col-xs-12">
+                      <form id="creatapprovalconcierge" name="creatapprovalconcierge" action="">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                          <label for="select_one_hotel" class="col-md-2 control-label">{{ trans('message.hotel') }}</label>
+                          <div class="col-md-10 selectContainer">
+                            <select id="select_one_hotel" name="select_one_hotel" class="form-control select2">
+                              <option value="" selected> Elija </option>
+                              @forelse ($hotels as $data_hotel)
+                                <option value="{{ $data_hotel->id }}"> {{ $data_hotel->Nombre_hotel }} </option>
+                              @empty
+                              @endforelse
+                            </select>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="select_one_type_rep" class="col-md-2 control-label">{{ trans('message.type') }}</label>
+                          <div class="col-md-10 selectContainer">
+                            <select id="select_one_type_rep" name="select_one_type_rep" class="form-control select2">
+                              <option value="" selected> Elija </option>
+                              @forelse ($types as $type)
+                                <option value="{{ $type->id }}"> {{ $type->name }} </option>
+                              @empty
+                              @endforelse
+                            </select>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  @else
+                    <div class="col-xs-12">
+                      @include('default.deniedmodule')
+                    </div>
+                  @endif
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              @if( auth()->user()->can('Create concierge approval') )
+                <button type="button" class="btn bg-navy create_user_data"><i class="fa fa-plus-square-o" style="margin-right: 4px;"></i>{{ trans('message.create') }}</button>
+              @endif
+              <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times" style="margin-right: 4px;"></i>{{ trans('message.ccmodal') }}</button>
+            </div>
+          </div>
+        </div>
+    </div>
 
     <div class="container">
         <div class="row">
@@ -33,9 +92,9 @@
               <div class="box box-solid">
                 <div class="box-body">
                   <div class="pull-right">
-                    <a href="javascript:void(0);" onclick="enviar_approval(this)"  class="btn btn-success" role="button" data-target="#add_approval">
+                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#add_approval">
                       <i class="fa fa-plus-square margin-r5"></i> Nueva Aprobación
-                    </a>
+                    </button>
                   </div>
                   <div class="row">
                     <div class="col-sm-12">
