0e108b8155d4fd537398cafd0487af53379d19f1
Unknown <esquinca_jose@hotmail.com>
2018-04-06 14:48:26 -0500

    Configuracion de correos
    envio de correos individuales.

--- app/Http/Controllers/ConfigurationSurveyController.php
+++ app/Http/Controllers/ConfigurationSurveyController.php
@@ -15,6 +15,7 @@ use DB;
 use Auth;
 use Mail;
 use App\Mail\Sentsurveynpsmail;
+use App\Mail\Sentsurveyrangelmail;
 use App\Hotel;
 use App\Encuesta;
 use App\Encuesta_user;
@@ -319,6 +320,64 @@ class ConfigurationSurveyController extends Controller
     $result = DB::table('encuesta_user_clientes')->get();
     //->orderBy('nombre', 'asc')
     return json_encode($result);
+  }
+
+  public function send_mail(Request $request)
+  {
+    $flag = 0;
+    $string1 = "";
+    $id_registro = $request->uh;
+    $res1 = DB::table('encuesta_user_clientes')->select('clientes', 'email', 'Special')->where('id_eu', $id_registro)->get();
+    $res2 = DB::table('encuesta_users')->select('user_id', 'shell_data', 'shell_status')->where('id', $id_registro)->get();
+
+    $name = $res1[0]->clientes;
+    $email = $res1[0]->email;
+    $status = $res1[0]->Special;
+
+    $user_id = $res2[0]->user_id;
+    $encriptodata = $res2[0]->shell_data;
+    $encriptostatus = $res2[0]->shell_status;
+
+    
+    if ($status === 0) {
+      $datos = [
+         'nombre' => $name,
+         'shell_data' => $encriptodata,
+         'shell_status' => $encriptostatus
+      ];
+
+      $this->sentSurveyEmail($email, $datos);
+      return $flag = 1;
+    }else{
+      $res3 = DB::select('CALL buscar_venue_user(?)', array($res2[0]->user_id));
+      $count = count($res3);
+
+      for ($i=0; $i < $count; $i++) { 
+          $string1 = $string1 . $res3[$i]->Nombre_hotel . ", ";
+      }
+      $string1 = substr($string1, 0, -2);
+
+      $datos = [
+         'nombre' => $name,
+         'string' => $string1,
+         'shell_data' => $encriptodata,
+         'shell_status' => $encriptostatus
+      ];
+
+      $this->sentSurveyEmailRangel($datos);
+      return $flag = 1;
+    }
+    return $flag;
+  }
+
+  public function sentSurveyEmailRangel($data)
+  {
+      // $datos = [
+      //    'nombre' => $sql[0]->name,
+      //    'shell_data' => $encriptodata,
+      //    'shell_status' => $encriptostatus
+      // ];
+      Mail::to('crangel@sitwifi.com')->send(new Sentsurveyrangelmail($data));
   }
 
   public function sentSurveyEmail($email, $data)
