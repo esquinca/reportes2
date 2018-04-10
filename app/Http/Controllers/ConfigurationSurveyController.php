9d3e71b65be06f2dc93abc9178ae4491bebba118
Unknown <esquinca_jose@hotmail.com>
2018-04-10 11:17:23 -0500

    cambios beta 2
    ...

--- app/Http/Controllers/ConfigurationSurveyController.php
+++ app/Http/Controllers/ConfigurationSurveyController.php
@@ -206,114 +206,123 @@ class ConfigurationSurveyController extends Controller
     $date_m= $request->month_evaluate;
     $operacion='0';
 
-    for ($i=0; $i < count($clientes); $i++) {
-      $month=$date_m.'-01';
-      $pregunto_a = DB::table('encuesta_users')
-                            ->where('user_id', $clientes[$i])
-                            ->where('encuesta_id', '1') // id encuesta
-                            ->where('estatus_id', '1') //Activa
-                            ->where('estatus_res', '0') //NO CONTESTADA
-                            ->where('fecha_corresponde', $month)
-                            ->count();
-      if ($pregunto_a == '0') {
-        //Pregunto b- Puede existir pero estar deshabilitada
-        $pregunto_b = DB::table('encuesta_users')
-                              ->where('user_id', $clientes[$i])
-                              ->where('encuesta_id', '1') // id encuesta
-                              ->where('estatus_id', '2') //deshabilitada
-                              ->where('estatus_res', '1') //CONTESTADA
-                              ->where('fecha_corresponde', $month)
-                              ->count();
-        if ($pregunto_b == '0') {
-          #ENTONCES VOLVEMOS A PREGUNTAR
-          $pregunto_c = DB::table('encuesta_users')
-                                ->where('user_id', $clientes[$i])
-                                ->where('encuesta_id', '1') // id encuesta
-                                ->where('estatus_id', '2') //deshabilitada
-                                ->where('estatus_res', '0') //NO CONTESTADA
-                                ->where('fecha_corresponde', $month)
-                                ->count();
-          if ($pregunto_c == '1') {
-            #ESTA DESHABILITADA PERO NO SE CONTESTO EN ESE PERIODO REGISTRO DE NUEVO
-            $nuevolink = $clientes[$i].'/'.'1'.'/'.$month.'/'.$date_e;
-            $encriptodata= Crypt::encryptString($nuevolink);
-            $encriptostatus= Crypt::encryptString('1');
-
-            $new_survey_individual = new Encuesta_user;
-            $new_survey_individual->user_id=$clientes[$i];
-            $new_survey_individual->encuesta_id='1';
-            $new_survey_individual->estatus_id='1';
-            $new_survey_individual->estatus_res='0';
-            $new_survey_individual->fecha_inicial=$date_i;
-            $new_survey_individual->fecha_corresponde=$month;
-            $new_survey_individual->fecha_fin=$date_e;
-            $new_survey_individual->shell_data=$encriptodata;
-            $new_survey_individual->shell_status=$encriptostatus;
-            $new_survey_individual->save();
-
-            $sql = DB::table('users')->select('email', 'name')->where('id', $input_emails[$i])->get();
-
-            $datos = [
-               'nombre' => $sql[0]->name,
-               'shell_data' => $encriptodata,
-               'shell_status' => $encriptostatus
-            ];
-            $this->sentSurveyEmail($sql[0]->email, $datos);
-            $operacion='1';
-
-          }
-          else{
-            #De plano no existe
-            $nuevolink = $clientes[$i].'/'.'1'.'/'.$month.'/'.$date_e;
-            $encriptodata= Crypt::encryptString($nuevolink);
-            $encriptostatus= Crypt::encryptString('1');
-
-            $new_survey_individual = new Encuesta_user;
-            $new_survey_individual->user_id=$clientes[$i];
-            $new_survey_individual->encuesta_id='1';
-            $new_survey_individual->estatus_id='1';
-            $new_survey_individual->estatus_res='0';
-            $new_survey_individual->fecha_inicial=$date_i;
-            $new_survey_individual->fecha_corresponde=$month;
-            $new_survey_individual->fecha_fin=$date_e;
-            $new_survey_individual->shell_data=$encriptodata;
-            $new_survey_individual->shell_status=$encriptostatus;
-            $new_survey_individual->save();
-            $sql = DB::table('users')->select('email', 'name')->where('id', $clientes[$i])->get();
-            $datos = [
-               'nombre' => $sql[0]->name,
-               'shell_data' => $encriptodata,
-               'shell_status' => $encriptostatus
-            ];
-            $this->sentSurveyEmail($sql[0]->email, $datos);
-            $operacion='1';
-          }
-        }
-        else {
-          #OPERACION ABORTADA POR QUE SE CONTESTO ESTE MES
-          $operacion='3';
-        }
-      }
-      else {
-        //Existe enlace -> Reenvio el link
-        $sql_data_user = DB::table('users')->select('email', 'name')->where('id', $clientes[$i])->get();
-        $data_pregunta = DB::table('encuesta_users')
+    if ($clientes == "" || $date_i == "" || $date_e = "" || $date_m = "") {
+      $operacion = '4';
+    }else{
+      for ($i=0; $i < count($clientes); $i++) {
+        $month=$date_m.'-01';
+        $pregunto_a = DB::table('encuesta_users')
                               ->where('user_id', $clientes[$i])
                               ->where('encuesta_id', '1') // id encuesta
                               ->where('estatus_id', '1') //Activa
                               ->where('estatus_res', '0') //NO CONTESTADA
                               ->where('fecha_corresponde', $month)
-                              ->get();
-        $datos = [
-           'nombre' => $sql_data_user[0]->name,
-           'shell_data' => $data_pregunta[0]->shell_data,
-           'shell_status' => $data_pregunta[0]->shell_status
-        ];
-        $this->sentSurveyEmail($sql_data_user[0]->email, $datos);
-        $operacion='2';
-      }
-    }
+                              ->count();
+        if ($pregunto_a == '0') {
+          //Pregunto b- Puede existir pero estar deshabilitada
+          $pregunto_b = DB::table('encuesta_users')
+                                ->where('user_id', $clientes[$i])
+                                ->where('encuesta_id', '1') // id encuesta
+                                ->where('estatus_id', '2') //deshabilitada
+                                ->where('estatus_res', '1') //CONTESTADA
+                                ->where('fecha_corresponde', $month)
+                                ->count();
+          if ($pregunto_b == '0') {
+            #ENTONCES VOLVEMOS A PREGUNTAR
+            $pregunto_c = DB::table('encuesta_users')
+                                  ->where('user_id', $clientes[$i])
+                                  ->where('encuesta_id', '1') // id encuesta
+                                  ->where('estatus_id', '2') //deshabilitada
+                                  ->where('estatus_res', '0') //NO CONTESTADA
+                                  ->where('fecha_corresponde', $month)
+                                  ->count();
+            if ($pregunto_c == '1') {
+              #ESTA DESHABILITADA PERO NO SE CONTESTO EN ESE PERIODO REGISTRO DE NUEVO
+              $nuevolink = $clientes[$i].'/'.'1'.'/'.$month.'/'.$date_e;
+              $encriptodata= Crypt::encryptString($nuevolink);
+              $encriptostatus= Crypt::encryptString('1');
 
+              $new_survey_individual = new Encuesta_user;
+              $new_survey_individual->user_id=$clientes[$i];
+              $new_survey_individual->encuesta_id='1';
+              $new_survey_individual->estatus_id='1';
+              $new_survey_individual->estatus_res='0';
+              $new_survey_individual->fecha_inicial=$date_i;
+              $new_survey_individual->fecha_corresponde=$month;
+              $new_survey_individual->fecha_fin=$date_e;
+              $new_survey_individual->shell_data=$encriptodata;
+              $new_survey_individual->shell_status=$encriptostatus;
+              $new_survey_individual->save();
+
+              $sql = DB::table('users')->select('email', 'name')->where('id', $input_emails[$i])->get();
+
+              $datos = [
+                 'nombre' => $sql[0]->name,
+                 'shell_data' => $encriptodata,
+                 'shell_status' => $encriptostatus
+              ];
+              $this->sentSurveyEmail($sql[0]->email, $datos);
+              $operacion='1';
+
+            }
+            else{
+              #De plano no existe
+              $nuevolink = $clientes[$i].'/'.'1'.'/'.$month.'/'.$date_e;
+              $encriptodata= Crypt::encryptString($nuevolink);
+              $encriptostatus= Crypt::encryptString('1');
+
+              $new_survey_individual = new Encuesta_user;
+              $new_survey_individual->user_id=$clientes[$i];
+              $new_survey_individual->encuesta_id='1';
+              $new_survey_individual->estatus_id='1';
+              $new_survey_individual->estatus_res='0';
+              $new_survey_individual->fecha_inicial=$date_i;
+              $new_survey_individual->fecha_corresponde=$month;
+              $new_survey_individual->fecha_fin=$date_e;
+              $new_survey_individual->shell_data=$encriptodata;
+              $new_survey_individual->shell_status=$encriptostatus;
+              $new_survey_individual->save();
+              $sql = DB::table('users')->select('email', 'name')->where('id', $clientes[$i])->get();
+              $datos = [
+                 'nombre' => $sql[0]->name,
+                 'shell_data' => $encriptodata,
+                 'shell_status' => $encriptostatus
+              ];
+              $this->sentSurveyEmail($sql[0]->email, $datos);
+              $operacion='1';
+            }
+          }
+          else {
+            #OPERACION ABORTADA POR QUE SE CONTESTO ESTE MES
+            $operacion='3';
+          }
+        }
+        else {
+          //Existe enlace -> Reenvio el link
+          $sql_data_user = DB::table('users')->select('email', 'name')->where('id', $clientes[$i])->get();
+          $data_pregunta = DB::table('encuesta_users')
+                                ->where('user_id', $clientes[$i])
+                                ->where('encuesta_id', '1') // id encuesta
+                                ->where('estatus_id', '1') //Activa
+                                ->where('estatus_res', '0') //NO CONTESTADA
+                                ->where('fecha_corresponde', $month)
+                                ->get();
+          $datos = [
+             'nombre' => $sql_data_user[0]->name,
+             'shell_data' => $data_pregunta[0]->shell_data,
+             'shell_status' => $data_pregunta[0]->shell_status
+          ];
+          $this->sentSurveyEmail($sql_data_user[0]->email, $datos);
+          $operacion='2';
+        }
+      }//end del for
+
+    }//end del else.
+
+    if ($operacion == '4') {
+        notificationMsg('danger', 'Favor de llenar todos los campos!');
+        return Redirect::back();
+    }
     if ($operacion == '3') {
         notificationMsg('danger', 'Mes, ya evaluado!');
         return Redirect::back();
